5199bae2e0ea9587bdf53d1fbb98d8d2cb49eb2d
Nataliia Tur <natalim@ukr.net>
2025-03-21 20:53:22 +0200

    blended 1

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,22 @@
 // import { FaRegThumbsUp } from 'react-icons/fa';
 // import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 // import { GiTreeDoor } from 'react-icons/gi';
-// import  StatisticsItem  from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import StatisticsItem from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-const Statistics = () => {
-  return <h2>Statistics</h2>;
+const Statistics = ({ stats }) => {
+  return (
+    <>
+      <h3 className={style.title}>Main Statistics</h3>
+      <ul className={style.list}>
+        {stats.map(item => (
+          <li className={style.item} key={item.id}>
+            <StatisticsItem total={item.total} title={item.title} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Statistics;
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,24 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './StatisticsItem.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { AiFillHeart } from 'react-icons/ai';
+
+const iconMap = {
+  'Happy Customers': <FaRegThumbsUp size={32} />,
+  'Registered Members': <MdPeople size={32} />,
+  'Available Products': <MdOutlineProductionQuantityLimits size={32} />,
+  'Saved Trees': <GiTreeDoor size={32} />,
+};
+
+const StatisticsItem = ({ total, title }) => {
+  return (
+    <>
+      {iconMap[title] || <AiFillHeart size={32} />}
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
